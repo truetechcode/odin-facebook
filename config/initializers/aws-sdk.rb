97f04dd17d0054f6c492d165fb0a285116949c86
jttmckee <wp.jason@hotmail.co.uk>
2019-03-11 21:55:57 +0000

    Fixing AWS

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,7 +1,13 @@
-require 'aws-sdk'
+require 'aws-sdk-s3'
 
 Aws.config.update({
   access_key_id: ENV['AWS_ACCESS_KEY_ID'] ,
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   region: 'eu-west-2'
+  #bucket: 'odin-book-jttm',
+  #service: 'S3'
+})
+
+Aws.config.update({
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'] ,ENV['AWS_SECRET_ACCESS_KEY'])
 })
